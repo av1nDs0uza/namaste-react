d5a4123b16713b2f3655aae9a6ba2b9c3ff7bf2b
av1nDs0uza <avinalricdsouza@gmail.com>
2024-11-06 18:39:04 +0530

    lec1 done

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+const parent = React.createElement(
+    "div", 
+    {id:"parent"},
+    React.createElement(
+        "div", 
+        {id:"child1"},
+        [React.createElement("h1", {},"I'm an h1 tag" ),
+         React.createElement("h2", {},"I'm an h2 tag" ),
+         React.createElement("h3", {},"I'm an h3 tag" ),
+         React.createElement("h4", {},"I'm an h4 tag" ),
+         React.createElement("h5", {},"I'm an h5 tag" ),
+    ]),
+    React.createElement(
+        "div", 
+        {id:"child2"},
+        [React.createElement("h1", {},"I'm an h1 tag" ),
+         React.createElement("h2", {},"I'm an h2 tag" ),
+         React.createElement("h3", {},"I'm an h3 tag" ),
+         React.createElement("h4", {},"I'm an h4 tag" ),
+         React.createElement("h5", {},"I'm an h5 tag" ),
+    ]),
+
+);
+
+
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
