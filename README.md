a1500932a32f86d3b9b82a33d84a77a34ce91cfb
av1nDs0uza <avinalricdsouza@gmail.com>
2024-11-13 17:07:22 +0530

    lec 2 complete

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # namaste-react
 learn react
+
+
+
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- caching - Faster Builds
+- IMage Optimization
+- Minification of File
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential bundling -  support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree shaking - remove unused code
+- Different dev and production bundles
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div", 
@@ -7,25 +7,26 @@ const parent = React.createElement(
     React.createElement(
         "div", 
         {id:"child1"},
-        [React.createElement("h1", {},"I'm an h1 tag" ),
-         React.createElement("h2", {},"I'm an h2 tag" ),
-         React.createElement("h3", {},"I'm an h3 tag" ),
-         React.createElement("h4", {},"I'm an h4 tag" ),
-         React.createElement("h5", {},"I'm an h5 tag" ),
-    ]),
+        [
+            React.createElement("h1", {}, "This is Namaste React"),
+            React.createElement("h2", {}, "I'm an h2 tag"),
+            React.createElement("h3", {}, "I'm an h3 tag"),
+            React.createElement("h4", {}, "I'm an h4 tag"),
+            React.createElement("h5", {}, "I'm an h5 tag"),
+        ]
+    ),
     React.createElement(
         "div", 
         {id:"child2"},
-        [React.createElement("h1", {},"I'm an h1 tag" ),
-         React.createElement("h2", {},"I'm an h2 tag" ),
-         React.createElement("h3", {},"I'm an h3 tag" ),
-         React.createElement("h4", {},"I'm an h4 tag" ),
-         React.createElement("h5", {},"I'm an h5 tag" ),
-    ]),
-
+        [
+            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h2", {}, "I'm an h2 tag"),
+            React.createElement("h3", {}, "I'm an h3 tag"),
+            React.createElement("h4", {}, "I'm an h4 tag"),
+            React.createElement("h5", {}, "I'm an h5 tag"),
+        ]
+    )
 );
-
-
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
